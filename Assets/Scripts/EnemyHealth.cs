eb5a249d486a196d9ed3dad449cfef4c1d9a79bb
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-08-19 01:21:22 +0700

    Damaging Enemies

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints = 5;
+    private int currentHitPoints = 0;
+
+    private void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+        
+        if (currentHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
