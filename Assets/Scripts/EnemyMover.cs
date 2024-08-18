3696f652297f19286ab7d130c72e356b64a059f4
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-08-18 19:36:37 +0700

    Models and enhance enemy movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] private float waitTime = 1.0f;
-
+    [SerializeField] [Range(0, 5)] private float speed = 1f;
+ 
     private void Start()
     {
         StartCoroutine(FollowPath());
@@ -16,8 +16,19 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (var waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPos = transform.position;
+            Vector3 endPos = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPos);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPos, endPos, travelPercent);
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
