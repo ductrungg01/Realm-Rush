51cae1f043e41da37cb30712515457f8dea2c581
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-08-18 19:46:33 +0700

    Detect User click the tile

--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (isPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
