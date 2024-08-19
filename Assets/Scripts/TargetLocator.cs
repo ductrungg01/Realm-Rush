75711655d7b0e7b1455b25e182e001f0684cf4a0
Vũ Đức Trung <vuductrung14112001@gmail.com>
2024-08-19 11:58:28 +0700

    Tower aim the closest enemy and inside it's attack range

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -7,18 +7,53 @@ public class TargetLocator : MonoBehaviour
     [SerializeField] private Transform weapon;
     private Transform target;
 
-    private void Start()
+    [SerializeField][Min(0)] private float attackRange = 15f;
+
+    private ParticleSystem projectileParticles;
+
+    private void Awake()
     {
-        target = FindObjectOfType<EnemyMover>().transform;
+        projectileParticles = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+
+        float minDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < minDistance)
+            {
+                minDistance = targetDistance;
+                closestTarget = enemy.transform;
+            }
+        }
+
+        target = closestTarget;
     }
 
     private void AimWeapon()
     {
+        float distance = Vector3.Distance (transform.position, target.transform.position);
+
+        Attack(distance <= attackRange);
+
         weapon.LookAt(target);
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
